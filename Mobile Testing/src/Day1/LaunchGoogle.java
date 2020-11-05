325edcc2e5214ffd5241e0b3f03ac6950ba8b000
ManoharChebrolu <ManoharChebrolu@Manohar-PC>
2020-11-05 10:31:43 +0530

    Second Push

--- Mobile Testing/src/Day1/LaunchGoogle.java
+++ Mobile Testing/src/Day1/LaunchGoogle.java
@@ -1,28 +1,23 @@
-package day1;
+package Day1;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
-public class LoginToDemoWebShop {
+public class LaunchGoogle {
 
 	public static void main(String[] args) throws MalformedURLException {
 		DesiredCapabilities capability = new DesiredCapabilities();
+		//capability.setCapability(MobileCapabilityType.DEVICE_NAME, "Manohar");
 		capability.setCapability("deviceName", "Manohar");
 		capability.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
 		capability.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
 		 AndroidDriver driver=new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"),capability);
-		driver.get("http://demowebshop.tricentis.com/login");
-        driver.findElement(By.xpath("//input[@id='Email']")).sendKeys("manzoorrocks191@gmail.com");
-        driver.findElement(By.xpath("//input[@id='Password']")).sendKeys("manipal");
-        driver.hideKeyboard();
-        driver.findElement(By.xpath("//input[@value='Log in']")).click();
-
+		driver.get("http://www.google.com");
 	}
 
 }
